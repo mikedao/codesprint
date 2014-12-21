863d79aec070839471e054d641a586ea0e2518c6
Michael Dao <michael.dao@gmail.com>
2014-12-20 22:59:44 -0700

    Update email.rb

--- email.rb
+++ email.rb
@@ -7,8 +7,9 @@
 def pull_email(data)
 	valid_email = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/
 	data.map 	{ |datum|
-	    datum.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => "")
-				}.collect{ |line| line.scan(valid_email)}	.flatten
+	    datum	.encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => "")
+			}.collect{ |line| line.scan(valid_email)}
+			.flatten
 															.uniq
 															.sort
 															.join(";")
