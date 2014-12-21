c217e3f108c053fd04dfdc649cff90c08c314f1b
Michael Dao <michael.dao@gmail.com>
2014-12-20 22:58:56 -0700

    Update digits.rb

--- digits.rb
+++ digits.rb
@@ -3,10 +3,10 @@
 
 def digitize(number)
 	number  .chars
-			.map(&:to_i)
-			.reject { |num| num == 0}
-			.map {|num| number.to_i % num == 0 ? 1 : 0}
-			.reduce(:+)						
+		.map(&:to_i)
+		.reject { |num| num == 0}
+		.map {|num| number.to_i % num == 0 ? 1 : 0}
+		.reduce(:+)						
 end
 
 #numbers.map {|number| digitize(number)}
